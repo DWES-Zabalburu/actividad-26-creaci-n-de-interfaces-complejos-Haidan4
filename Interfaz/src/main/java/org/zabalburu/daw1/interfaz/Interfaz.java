33529f52ed9f77e6295e0822d52d306bcd970ad8
Haidan4 <aidan.boada.perez@zabalburu.org>
2025-04-08 13:55:42 +0200

    Va por buen camino

---
+++ Interfaz/src/main/java/org/zabalburu/daw1/interfaz/Interfaz.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.zabalburu.daw1.interfaz;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author DAW1
+ */
+public class Interfaz {
+
+    public static void main(String[] args) {
+        new VentanaInterfaz().setVisible(true);
+    }
+}
